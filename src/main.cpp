aadce1e50a346c3939f66bc637005be9462d0d5f
Tim Deve <timdeve@users.noreply.github.com>
2019-08-24 19:41:54 +0100

    Makes the screen turn off after a few seconds of inactivity

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <M5Stack.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <Ticker.h>
 #include <secrets.h>
 
 const char *networkName = NETWORK_NAME;
@@ -10,14 +11,21 @@ const char *udpAddress = "10.0.0.255";
 const int udpPort = 56700;
 
 boolean isWifiConnected = false;
+boolean screenOn = true;
+boolean tickerAttached = false;
+uint tickSinceScreenWakeUp = 0;
 
 WiFiUDP udp;
+Ticker screenTicker;
 
 const unsigned char turnOnMessage[] = {0x28, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x01, 0x00, 0x00};
 const unsigned char turnOffMessage[] = {0x28, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00};
 
 void wifiEventHandler(WiFiEvent_t event);
 void connectToWiFi(const char *ssid, const char *pwd);
+void screenTimeout();
+void turnScreenOff();
+void turnScreenOn();
 
 void setup()
 {
@@ -37,6 +45,8 @@ void loop()
 
   if (M5.BtnA.wasReleased())
   {
+    turnScreenOn();
+
     if (isWifiConnected)
     {
       udp.beginPacket(udpAddress, udpPort);
@@ -50,6 +60,8 @@ void loop()
   }
   else if (M5.BtnB.wasReleased())
   {
+    turnScreenOn();
+
     if (isWifiConnected)
     {
       udp.beginPacket(udpAddress, udpPort);
@@ -87,6 +99,12 @@ void wifiEventHandler(WiFiEvent_t event)
 
     udp.begin(WiFi.localIP(), udpPort);
 
+    if (!tickerAttached)
+    {
+      screenTicker.attach(1, screenTimeout);
+      tickerAttached = true;
+    }
+
     isWifiConnected = true;
     break;
   case SYSTEM_EVENT_STA_DISCONNECTED:
@@ -97,5 +115,36 @@ void wifiEventHandler(WiFiEvent_t event)
   default:
     // Other events; Do nothing
     break;
+  }
+}
+
+void screenTimeout()
+{
+  if (tickSinceScreenWakeUp > 3 && screenOn)
+  {
+    turnScreenOff();
+  }
+  else if (screenOn)
+  {
+    tickSinceScreenWakeUp += 1;
+  }
+}
+
+void turnScreenOff()
+{
+  screenOn = false;
+
+  M5.Lcd.setBrightness(0);
+}
+
+void turnScreenOn()
+{
+  tickSinceScreenWakeUp = 0;
+
+  if (screenOn == false)
+  {
+    screenOn = true;
+
+    M5.Lcd.setBrightness(200);
   }
 }
